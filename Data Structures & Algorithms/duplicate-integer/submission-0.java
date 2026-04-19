a33495573351959779053a09e9b8ce57dbf5044c
duynd0909 <72845301+duynd0909@users.noreply.github.com>
2026-04-19 22:30:13 +0700

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        Set<Integer> cache = new HashSet<>();
+        for(int i=0; i< nums.length;i++){
+            if(cache.contains(nums[i])){
+                return true;
+            }
+            cache.add(nums[i]);
+        }
+        return false;
+    }
+}
