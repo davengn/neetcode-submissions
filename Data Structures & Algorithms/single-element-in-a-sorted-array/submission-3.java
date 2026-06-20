f3c767ae7645d6fd74af09fabaf93555608eac77
Dave Nguyen <72845301+davengn@users.noreply.github.com>
2026-06-20 08:48:39 +0000

    Add: single-element-in-a-sorted-array - submission-3

---
+++ Data Structures & Algorithms/single-element-in-a-sorted-array/submission-3.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int n =nums.length;
+        for(int i = 0; i< n;i++){
+            if(i ==0 || nums[i] == nums[i-1] || (i < n-1 && nums[i] == nums[i+1])){
+                continue;
+            }
+            return nums[i];
+        }
+        return nums[0];
+    }
+}
