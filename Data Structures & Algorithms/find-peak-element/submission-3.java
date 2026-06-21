5ac71ab76cd2810c76fa164f97dba776590127da
Dave Nguyen <72845301+davengn@users.noreply.github.com>
2026-06-21 07:49:24 +0000

    Add: find-peak-element - submission-3

---
+++ Data Structures & Algorithms/find-peak-element/submission-3.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int n = nums.length;
+        int left = 0;
+        int right = n - 1;
+        int res = 0;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (mid > 0 && nums[mid] < nums[mid - 1]) {
+                right = mid - 1;
+            } else if (mid < n - 1 && nums[mid] < nums[mid + 1]) {
+                left = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return res;
+    }
+}
