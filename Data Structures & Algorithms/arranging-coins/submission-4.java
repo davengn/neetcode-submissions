9a67c6c8be3a1524401a1a848c7aba545469cf46
Dave Nguyen <72845301+davengn@users.noreply.github.com>
2026-06-24 02:50:01 +0000

    Add: arranging-coins - submission-4

---
+++ Data Structures & Algorithms/arranging-coins/submission-4.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int arrangeCoins(int n) {
+        int l = 1;
+        int r = n;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            long cost = (long) m * (m + 1)/2;
+            if(cost <= n){
+                l = m +1;
+            }else{
+                r= m -1;
+            }
+        }
+        return l -1;
+    }
+}
