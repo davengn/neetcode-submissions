6f882127ca96a78966dc3cd0adc30dccc6a4e082
Dave Nguyen <72845301+davengn@users.noreply.github.com>
2026-06-29 03:45:07 +0000

    Add: island-perimeter - submission-1

---
+++ Data Structures & Algorithms/island-perimeter/submission-1.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int islandPerimeter(int[][] grid) {
+              int rows = grid.length;
+        int cols = grid[0].length;
+        int startR = -1, startC = -1;
+        for (int r = 0; r < rows && startR == -1; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (grid[r][c] == 1) {
+                    startR = r;
+                    startC = c;
+                    break;
+                }
+            }
+        }
+  
+        Stack<Pair<Integer, Integer>> stack = new Stack<>();
+        Set<Pair<Integer, Integer>> visited = new HashSet<>();
+        stack.push(new Pair<>(startR, startC));
+        int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        int perimeter = 0;
+        while (!stack.isEmpty()) {
+            Pair<Integer, Integer> node = stack.pop();
+            int r = node.getKey();
+            int c = node.getValue();
+            if (visited.contains(node))
+                continue;
+            if (r < 0 || r >= rows || c < 0 || c >= cols) {
+                perimeter++;
+                continue;
+            }
+            if (grid[r][c] == 0) {
+                perimeter++;
+                continue;
+            }
+            visited.add(node);
+            for (int[] dir : directions) {
+                int newRow = r + dir[0];
+                int newCol = c + dir[1];
+
+                stack.push(new Pair<Integer, Integer>(newRow, newCol));
+            }
+        }
+        return perimeter;
+    }
+}
