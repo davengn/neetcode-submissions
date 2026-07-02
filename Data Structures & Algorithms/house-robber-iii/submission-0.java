166d5c49fd864105e8069f4369a712f54bdac323
Dave Nguyen <72845301+davengn@users.noreply.github.com>
2026-07-02 04:42:52 +0000

    Add: house-robber-iii - submission-0

---
+++ Data Structures & Algorithms/house-robber-iii/submission-0.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {} 
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int rob(TreeNode root) {
+        int [] res = dfs(root);
+        return Math.max(res[0],res[1]);
+    }
+    private int[] dfs(TreeNode node){
+        if(node == null) return new int[]{0,0};
+        int[] left = dfs(node.left);
+        int[] right = dfs(node.right);
+        int robbeb = node.val + left[1] + right[1];
+        int notRobbeb = Math.max(left[0],left[1]) + Math.max(right[0],right[1]);
+        return new int[]{robbeb,notRobbeb};
+    }
+}
