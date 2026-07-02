6bf077ba1ed0fa70e9591944ebc3150e7c4642b8
Dave Nguyen <72845301+davengn@users.noreply.github.com>
2026-07-02 03:36:47 +0000

    Add: sum-root-to-leaf-numbers - submission-0

---
+++ Data Structures & Algorithms/sum-root-to-leaf-numbers/submission-0.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int sumNumbers(TreeNode root) {
+        Stack<Pair<TreeNode, Integer>> stack = new Stack<>();
+        stack.push(new Pair<TreeNode, Integer>(root, 0));
+        int sum = 0;
+        while (!stack.isEmpty()) {
+            Pair<TreeNode, Integer> pair = stack.pop();
+            TreeNode node = pair.getKey();
+            int pathSum = pair.getValue();
+
+            String combinedStr = ""+ pathSum + node.val;
+            pathSum = Integer.parseInt(combinedStr);
+            if (node.left == null && node.right == null) {
+                sum += pathSum;
+            }
+            if (node.left != null) {
+                stack.push(new Pair<TreeNode, Integer>(node.left, pathSum));
+            }
+            if (node.right != null) {
+                stack.push(new Pair<TreeNode, Integer>(node.right, pathSum));
+            }
+        }
+        return sum;
+    }
+}
