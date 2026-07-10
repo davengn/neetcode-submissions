f3b78012cd648e69c92e4816d96a7eaed7524081
Dave Nguyen <72845301+davengn@users.noreply.github.com>
2026-07-10 10:34:56 +0700

    Add: contains-duplicate-ii - submission-2

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-2.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer,Integer> cache = new HashMap<>();
+        for(int i = 0 ; i < nums.length; i++){
+            if(cache.containsKey(nums[i]) && i - cache.get(nums[i]) <= k){
+                return true;
+            }
+            cache.put(nums[i],i);
+        }
+        return false;
+    }
+}
