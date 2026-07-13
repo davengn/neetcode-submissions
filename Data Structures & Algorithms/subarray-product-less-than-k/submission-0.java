886950919bbc8168b30d4a51729ff097e9297706
Dave Nguyen <72845301+davengn@users.noreply.github.com>
2026-07-13 11:41:01 +0700

    Add: subarray-product-less-than-k - submission-0

---
+++ Data Structures & Algorithms/subarray-product-less-than-k/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        int l = 0;
+        int count = 0;
+        int product = 1;
+        for (int r = 0; r < nums.length; r++) {
+            product *= nums[r];
+            while (product >= k && l <= r) {
+                product /= nums[l];
+                l++;
+            }
+            count += (r - l + 1);
+        }
+        return count;
+    }
+}
